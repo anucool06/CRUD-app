5982a51e55e7561ab002ab08f8ae5d791abcb994
Anubhav Srivastava <anucool06@gmail.com>
2022-01-22 19:03:03 +0530

    final

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,4 +1,5 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/reactdb'
+    // db: 'mongodb://localhost:27017/reactdb'
+    db: 'mongodb+srv://user:userpassword123@crud-app.hbbtp.mongodb.net/Crud-app?retryWrites=true&w=majority'
     };
     
